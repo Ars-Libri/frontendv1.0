87f998845d5f6ff07ceecd5833836e01b9adb36f
Pedro Ribeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-07-21 19:57:21 -0300

    change transition(menu)

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 function openMenu(){
     document.getElementById('header-page').style.height = '0';
     document.getElementById('header-page').style.width = '0';
-    document.getElementById('header-page').style.marginTop = '100vh';
+    document.getElementById('header-page').style.marginLeft = '100vw';
+    document.getElementById('main-page').style.marginLeft = '100vw';
+    document.getElementById('section-page').style.marginLeft = '100vw';
+    document.getElementById('footer-page').style.marginLeft = '100vw';
     document.getElementById('main-page').style.height = '0';
     document.getElementById('main-page').style.width = '0';
     document.getElementById('section-page').style.height = '0';
     document.getElementById('section-page').style.width = '0';
+    document.getElementById('footer-page').style.width = '0';
+    document.getElementById('footer-page').style.height = '0';
     document.getElementById('nav-general').style.width = '100vw';
     document.getElementById('nav-general').style.height = 'auto';
 }
 function closeMenu(){
     document.getElementById('header-page').style.height = 'auto';
     document.getElementById('header-page').style.width = 'auto';
-    document.getElementById('header-page').style.marginTop = '0vh';
+    document.getElementById('header-page').style.marginLeft = '0vw';
+    document.getElementById('main-page').style.marginLeft = '0vw';
+    document.getElementById('section-page').style.marginLeft = '0vw';
+    document.getElementById('footer-page').style.marginLeft = '0vw';
     document.getElementById('main-page').style.height = 'auto';
     document.getElementById('main-page').style.width = 'auto';
     document.getElementById('section-page').style.height = 'auto';
     document.getElementById('section-page').style.width = 'auto';
+    document.getElementById('footer-page').style.width = 'auto';
+    document.getElementById('footer-page').style.height = 'auto';
     document.getElementById('nav-general').style.width = '0';
     document.getElementById('nav-general').style.height = '0';
 }
