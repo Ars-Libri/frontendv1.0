a508654a63153ddeb2f5fe49d656823074240e89
Pedro Ribeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-07-21 23:49:41 -0300

    home page done - about us done

---
+++ sobre-nos/script.js
@@ -0,0 +1,52 @@
+function openMenu(){
+    document.getElementById('header-top').style.height = '0';
+    document.getElementById('header-top').style.width = '0';
+    document.getElementById('header-main').style.height = '0';
+    document.getElementById('header-main').style.width = '0';
+    document.getElementById('texts').style.width = '0';
+    document.getElementById('texts').style.height = '0';
+    document.getElementById('elipse').style.width = '0';
+    document.getElementById('elipse').style.height = '0';
+    document.getElementById('header-bottom').style.height = '0';
+    document.getElementById('header-bottom').style.width = '0';
+    document.getElementById('header-top').style.marginLeft = '100vw';
+    document.getElementById('header-main').style.marginLeft = '100vw';
+    document.getElementById('header-bottom').style.marginLeft = '100vw';
+    // document.getElementById('main-page').style.marginLeft = '100vw';
+    // document.getElementById('section-page').style.marginLeft = '100vw';
+    // document.getElementById('footer-page').style.marginLeft = '100vw';
+    // document.getElementById('main-page').style.height = '0';
+    // document.getElementById('main-page').style.width = '0';
+    // document.getElementById('section-page').style.height = '0';
+    // document.getElementById('section-page').style.width = '0';
+    // document.getElementById('footer-page').style.width = '0';
+    // document.getElementById('footer-page').style.height = '0';
+    document.getElementById('nav-general').style.width = '100vw';
+    document.getElementById('nav-general').style.height = 'auto';
+}
+function closeMenu(){
+    document.getElementById('header-top').style.height = '12vh';
+    document.getElementById('header-top').style.width = 'auto';
+    document.getElementById('header-main').style.height = '76vh';
+    document.getElementById('header-main').style.width = 'auto';
+    document.getElementById('texts').style.width = 'auto';
+    document.getElementById('texts').style.height = 'auto';
+    document.getElementById('elipse').style.width = '12rem';
+    document.getElementById('elipse').style.height = '12rem';
+    document.getElementById('header-bottom').style.height = '12vh';
+    document.getElementById('header-bottom').style.width = 'auto';
+    document.getElementById('header-top').style.marginLeft = '0vw';
+    document.getElementById('header-main').style.marginLeft = '0vw';
+    document.getElementById('header-bottom').style.marginLeft = '0vw';
+    // document.getElementById('main-page').style.marginLeft = '0vw';
+    // document.getElementById('section-page').style.marginLeft = '0vw';
+    // document.getElementById('footer-page').style.marginLeft = '0vw';
+    // document.getElementById('main-page').style.height = 'auto';
+    // document.getElementById('main-page').style.width = 'auto';
+    // document.getElementById('section-page').style.height = 'auto';
+    // document.getElementById('section-page').style.width = 'auto';
+    // document.getElementById('footer-page').style.width = 'auto';
+    // document.getElementById('footer-page').style.height = 'auto';
+    document.getElementById('nav-general').style.width = '0';
+    document.getElementById('nav-general').style.height = '0';
+}
