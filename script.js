69ad687448ed1f8fda8baa423a1c618e9cac0ab2
Pedro Ribeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-08-06 17:46:52 -0300

    slider done

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function openMenu(){
     document.getElementById('header-page').style.width = '0';
     document.getElementById('header-page').style.marginLeft = '100vw';
     document.getElementById('main-page').style.marginLeft = '100vw';
-    document.getElementById('section-page').style.marginLeft = '100vw';
+    document.getElementById('section-category').style.marginLeft = '100vw';
     document.getElementById('footer-page').style.marginLeft = '100vw';
     document.getElementById('main-page').style.height = '0';
     document.getElementById('main-page').style.width = '0';
-    document.getElementById('section-page').style.height = '0';
-    document.getElementById('section-page').style.width = '0';
+    document.getElementById('section-category').style.height = '0';
+    document.getElementById('section-category').style.width = '0';
+    document.getElementById('quadrado-de-fundo').style.width = '0';
+    document.getElementById('quadrado-de-fundo').style.height = '0';
     document.getElementById('footer-page').style.width = '0';
     document.getElementById('footer-page').style.height = '0';
     document.getElementById('nav-general').style.width = '100vw';
@@ -18,13 +20,15 @@ function closeMenu(){
     document.getElementById('header-page').style.height = 'auto';
     document.getElementById('header-page').style.width = 'auto';
     document.getElementById('header-page').style.marginLeft = '0vw';
+    document.getElementById('quadrado-de-fundo').style.width = '150px';
+    document.getElementById('quadrado-de-fundo').style.height = '150px';
     document.getElementById('main-page').style.marginLeft = '0vw';
-    document.getElementById('section-page').style.marginLeft = '0vw';
+    document.getElementById('section-category').style.marginLeft = '0vw';
     document.getElementById('footer-page').style.marginLeft = '0vw';
     document.getElementById('main-page').style.height = 'auto';
     document.getElementById('main-page').style.width = 'auto';
-    document.getElementById('section-page').style.height = 'auto';
-    document.getElementById('section-page').style.width = 'auto';
+    document.getElementById('section-category').style.height = 'auto';
+    document.getElementById('section-category').style.width = 'auto';
     document.getElementById('footer-page').style.width = 'auto';
     document.getElementById('footer-page').style.height = 'auto';
     document.getElementById('nav-general').style.width = '0';
@@ -39,3 +43,43 @@ function colorBtnNav(id){
         }
     }
 }
+/* Script slider */
+
+/* Responsive CSS slider */
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
