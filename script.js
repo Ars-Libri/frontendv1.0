451342dfaaed7c5cb838062623bc16405e43b0fe
Pedro Ribeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-08-08 15:48:12 -0300

    home updates, slider done

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 function openMenu(){
-    document.getElementById('header-page').style.height = '0';
-    document.getElementById('header-page').style.width = '0';
-    document.getElementById('header-page').style.marginLeft = '100vw';
-    document.getElementById('main-page').style.marginLeft = '100vw';
-    document.getElementById('section-category').style.marginLeft = '100vw';
-    document.getElementById('footer-page').style.marginLeft = '100vw';
-    document.getElementById('main-page').style.height = '0';
-    document.getElementById('main-page').style.width = '0';
-    document.getElementById('section-category').style.height = '0';
-    document.getElementById('section-category').style.width = '0';
-    document.getElementById('quadrado-de-fundo').style.width = '0';
-    document.getElementById('quadrado-de-fundo').style.height = '0';
-    document.getElementById('footer-page').style.width = '0';
-    document.getElementById('footer-page').style.height = '0';
-    document.getElementById('nav-general').style.width = '100vw';
-    document.getElementById('nav-general').style.height = 'auto';
+  var textsAbsolute = document.getElementsByClassName('texts-absolute');
+
+  for(var i = 0;i<textsAbsolute.length;i++){
+    textsAbsolute[i].style.width = '0';
+  }
+
+  //
+
+  var arrowsSlider = document.getElementsByClassName('arrow-slide');
+
+  for(var i = 0;i<arrowsSlider.length;i++){
+    arrowsSlider[i].style.width = '0';
+  }
+
+  //
+
+  var elementsHidden = document.getElementsByClassName('hidden');
+
+  for(var i = 0;i<elementsHidden.length;i++){
+    elementsHidden[i].style.width = '0';
+    elementsHidden[i].style.height = '0';
+  }
+
+  //
+
+  document.getElementById('quadrado-de-fundo').style.width = '0';
+  document.getElementById('quadrado-de-fundo').style.height = '0';
+  document.getElementById('nav-general').style.width = '100vw';
+  document.getElementById('nav-general').style.height = 'auto';
 }
 function closeMenu(){
-    document.getElementById('header-page').style.height = 'auto';
-    document.getElementById('header-page').style.width = 'auto';
-    document.getElementById('header-page').style.marginLeft = '0vw';
-    document.getElementById('quadrado-de-fundo').style.width = '150px';
-    document.getElementById('quadrado-de-fundo').style.height = '150px';
-    document.getElementById('main-page').style.marginLeft = '0vw';
-    document.getElementById('section-category').style.marginLeft = '0vw';
-    document.getElementById('footer-page').style.marginLeft = '0vw';
-    document.getElementById('main-page').style.height = 'auto';
-    document.getElementById('main-page').style.width = 'auto';
-    document.getElementById('section-category').style.height = 'auto';
-    document.getElementById('section-category').style.width = 'auto';
-    document.getElementById('footer-page').style.width = 'auto';
-    document.getElementById('footer-page').style.height = 'auto';
-    document.getElementById('nav-general').style.width = '0';
-    document.getElementById('nav-general').style.height = '0';
+  var textsAbsolute = document.getElementsByClassName('texts-absolute');
+
+  for(var i = 0;i<textsAbsolute.length;i++){
+    textsAbsolute[i].style.width = 'auto';
+  }
+
+  //
+
+  var arrowsSlider = document.getElementsByClassName('arrow-slide');
+
+  for(var i = 0;i<arrowsSlider.length;i++){
+    arrowsSlider[i].style.width = '13vw';
+  }
+
+  //
+
+  var elementsHidden = document.getElementsByClassName('hidden');
+
+  for(var i = 0;i<elementsHidden.length;i++){
+    elementsHidden[i].style.width = '100vw';
+    elementsHidden[i].style.height = 'auto';
+  }
+
+  //
+
+  document.getElementById('quadrado-de-fundo').style.width = '150px';
+  document.getElementById('quadrado-de-fundo').style.height = '150px';
+  document.getElementById('nav-general').style.width = '0';
+  document.getElementById('nav-general').style.height = '0';
 }
 function colorBtnNav(id){
-    for(var i = 1;i<5;i++){
-        if(id==i){
-            var element = document.getElementById("categoria-"+i);
-            element.classList.add('btn-select-category');
-        } else{
-          var element = document.getElementById("categoria-"+i);
-          element.classList.remove('btn-select-category');
-        }
+  for(var i = 1;i<5;i++){
+    if(id==i){
+      var element = document.getElementById("categoria-"+i);
+      element.classList.add('btn-select-category');
+    } else{
+      var element = document.getElementById("categoria-"+i);
+      element.classList.remove('btn-select-category');
     }
+  }
 }
 /* Script slider */
-
-/* Responsive CSS slider */
 
 const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
@@ -85,3 +109,5 @@ function prevSlider() {
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
+
+setInterval(nextSlider, 7000);
